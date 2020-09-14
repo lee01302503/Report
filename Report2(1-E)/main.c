0c32f9b31688fa4b0e4bb5220aae27c90ead2656
lee01302503 <lee01302503@naver.com>
2020-09-14 15:16:35 +0900

    Report2(1-E)

---
+++ Report2(1-E)/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  int a, b, tot;
+  double avg;
+
+  printf("Two subject score : ");
+  scanf("%d%d", &a, &b);
+  tot = a + b;
+  avg = tot / 2.0;
+
+  printf("Avg : %.1lf\n", avg);
+
+  return 0;
+}

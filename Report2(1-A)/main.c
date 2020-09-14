01413c032c01160e3596282b6ecb235d3166248c
lee01302503 <lee01302503@naver.com>
2020-09-14 11:57:52 +0900

    Report2(1-A)

---
+++ Report2(1-A)/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  printf("Dividing %d by %d gives you %.6lf\n", 1, 2, 0.5);
+  return 0;
+
+}

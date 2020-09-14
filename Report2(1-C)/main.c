8944a9e291f3fa50e56f058d1a04c302a17799a5
lee01302503 <lee01302503@naver.com>
2020-09-14 12:15:30 +0900

    Report2(1-c . 2)

---
+++ Report2(1-C)/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  int a = 0;
+
+  a = a + 1;
+  a = a + 2;
+  a = a + 3;
+  printf("a : %d", a);
+
+  return 0;
+}

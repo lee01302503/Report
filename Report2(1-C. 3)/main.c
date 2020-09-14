c773a8ccdf7cf3fbfd9559196ea80cb903961c6d
lee01302503 <lee01302503@naver.com>
2020-09-14 12:26:16 +0900

    Report2(1-C. 3)

---
+++ Report2(1-C. 3)/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  int a = 70, b = 80, c = 90;
+  int tot = 0;
+
+  tot = a + b + c;
+  printf("Korea : %d, English : %d, Math : %d\n", a, b, c);
+  printf("Toatol : %d\n", tot);
+}

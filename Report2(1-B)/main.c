5ab06a9eff603c9595865dea7d06d6ab5eaf648f
lee01302503 <lee01302503@naver.com>
2020-09-14 12:10:28 +0900

    Report2(1-B)

---
+++ Report2(1-B)/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  printf("Num : %d\n", 32165);
+  printf("Name : %s\n", "Gildong Hong");
+  printf("Grade : %c\n", 'A');
+
+  return 0;
+}

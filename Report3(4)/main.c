a83f43ffc6abddaa6202ee50369be14cdf60a555
lee01302503 <lee01302503@naver.com>
2020-09-22 15:38:59 +0900

    Report3(4)

---
+++ Report3(4)/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define divisor 0
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  int a, b, res;
+
+  printf("intger number : ");
+  scanf("%d%d", &a, &b);
+  res = (b % a);
+
+  switch (res)
+  {
+  case divisor:
+	printf("%d is a factor of %d\n", a, b);
+	break;
+  default:
+	printf("%d is not a factor of %d\n", a, b);
+  }
+}

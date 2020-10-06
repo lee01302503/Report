2d2ef26f6212dd6691a576e347dd97525254c89e
lee01302503 <lee01302503@naver.com>
2020-10-06 13:50:57 +0900

    Project1(1-fix)

--- Project1(1)/main.c
+++ Project1(1)/main.c
@@ -9,18 +9,10 @@ int main(void)
   printf("enter input degree : ");//Angle input
   scanf("%lf", &a);
 
-  if (a >= 360.0)//More than 360 degrees
-  {
-	a -= (((int)a / 360) * 360.0);//Divide the number you entered by 360 degrees, multiply the quotient by 360, and store it in the variable a
-	res = a - 180.0;//Subtract 180 again from the stored variable a.
-	printf("%.1lf", res);//Output the result value
-  }
-
-  else//Less than 360 degrees
-  {
-	res = a - 180.0;//Subtract 180 degrees from the input value
-	printf("%.1lf", res);//Output the result value
-  }
+  res = (((int)a) % 360) - 180.0;
+  res += a - (int)a;
+  
+  printf("%.1lf = %.1lf", a, res);
 
   return 0;
 }

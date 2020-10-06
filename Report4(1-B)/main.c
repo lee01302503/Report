5079e425b6241c85bec00881f8bcb7bf5ad15854
lee01302503 <lee01302503@naver.com>
2020-10-06 12:33:20 +0900

    Report4(1-B)

---
+++ Report4(1-B)/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* made by Prof. Lee
+*/
+int main(void)
+{
+  int i;
+
+  for (i = 0; i < 5; i++)
+  {
+	printf("Be happy\n");
+  }
+
+  return 0;
+}

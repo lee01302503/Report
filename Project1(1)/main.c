9711d6852339319c79e6fe40c0b7140d5801f28f
lee01302503 <lee01302503@naver.com>
2020-10-08 11:49:48 +0900

    Project1(1-fixed)

--- Project1(1)/main.c
+++ Project1(1)/main.c
@@ -9,10 +9,10 @@ int main(void)
   printf("enter input degree : ");//Angle input
   scanf("%lf", &a);
 
-  res = (((int)a) % 360) - 180.0;
-  res += a - (int)a;
+  res = (((int)a) % 360) - 180.0;//Converts the entered value from - 180 to 179
   
-  printf("%.1lf = %.1lf", a, res);
+  
+  printf("%.1lf = %.1lf", a, res);//Output conversion value
 
   return 0;
 }

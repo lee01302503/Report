23a2bd081702875a9954123b75a680d00e43ec18
lee01302503 <lee01302503@naver.com>
2020-10-21 00:54:31 +0900

    Report5(3)

---
+++ Report5(3)/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+/* made by Prof. Lee
+*/
+int nRandom(void);
+
+int main(void)
+{
+  int a;
+  char b;
+  while (1)
+  {
+	printf("frontside(1), backside(0) : ");
+	scanf("%d", &a);
+	if ((0 <= a) && (a <= 1))// Execute when the entered number is 1 and 0
+	{
+	  if (a == nRandom())//Matches any number
+	  {
+		printf("Success!!!\n");
+	  }
+	  else//Doesn't match any number.
+	  {
+		printf("Different....\n");
+	  }
+	  printf("continue???( y or n)");
+	  getchar();//clean buffer
+	  scanf("%c", &b);
+	  if (b == 'n')
+		break;
+	  else if (b == 'y')
+		continue;
+	}
+	else
+	{
+	  printf("Please check number....\n");
+	}
+	
+  }
+}
+
+int nRandom(void)//Specify random number function
+{
+  int a;
+  srand((unsigned)(time(NULL)));
+  a = rand() % 2;//Takes 0 and 1 using the coefficient operator.
+  return a;
+}

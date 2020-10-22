f88e8dc0e3bf3936032eaf9ec5328542f4ced3f2
lee01302503 <lee01302503@naver.com>
2020-10-22 20:38:31 +0900

    Report6(2-pratice problem)

---
+++ Report6(2-pratice)/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include<time.h>
+
+
+/* made by Prof. Lee
+*/
+int get_rand_int(void);//Random number function declaration
+void swap(int* pa, int* pb);//Declare the swap function
+void line_up(int a[]);//line up function declaration
+
+int main(void)
+{
+  int a[10];
+  int i;
+
+  printf("\nRandom number :\t");
+  for (i = 0; i < 10; i++)
+  {
+    a[i] = get_rand_int();//Generate random function
+    printf("%3d", a[i]);
+  }
+  line_up(a);
+
+
+  printf("\n\nSorted numbers :\t");
+  for (i = 0; i < 10; i++)
+  {
+    printf("%3d", a[i]);//Random number sort
+  }
+
+
+  return 0;
+}
+
+int get_rand_int(void)
+{
+  Sleep(10);
+  srand(GetTickCount());
+  return rand() % 10;
+}
+
+void swap(int* pa, int* pb)
+{
+  int temp;
+
+  temp = *pa;
+  *pa = *pb;
+  *pb = temp;
+}
+
+void line_up(int a[])
+{
+  int i, j;
+
+  for (i = 10 - 1; i > 0; i--)//Bubble sort
+  {
+    for (j = 0; j < i; j++)
+    {
+      if (a[j] > a[j + 1])
+      {
+        swap(&a[j], &a[j + 1]);
+      }
+    }
+  }
+}
